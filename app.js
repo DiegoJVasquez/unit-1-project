fcfc1a01f13f4c5737ec8ea103a11f1e74d6a24a
DiegoJVasquez <vasquezcrew707@gmail.com>
2023-05-12 00:50:09 -0700

    finishing the game basics

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-///   IPO-> INPUT, PROCESS AND OUTPUT   ///
+
 
 ///   Constants data that does NOT change   ///
   const solutions= [
@@ -14,8 +14,8 @@
 
   const PLAYERS= {
     'null' : '',
-    '1' : 'X',
-    '-1' : 'O',
+    '1' : 'X',                   ///player 1 is X
+    '-1' : 'O',                 ///player 2 is O
   };
 
   /// variables - data that does change   ///
@@ -32,29 +32,47 @@ gameboardEl.addEventListener('click', handleClick);
 buttonEl.addEventListener('click', init);
 
  ///   functions   ///
-init();                                                   ///starts and resets the game
+init();                                                      ///starts and resets the game
   
 function init() {
-  turn= 1;                                              /// X will start
-  winner= false;                                       ///no winner currently
-  gameboard = new Array(9).fill(null)                 /// create emtpy gameboard
-  render();                                          /// updates the game dom new   Array(9).fill(null);
+  turn= 1;                                                     /// X will start
+  winner= false;                                              ///no winner currently
+  gameboard = new Array(9).fill(null)                        /// create emtpy gameboard
+  render();                                                 /// updates the game dom 
 };
 
+function getWinner(){
+for(let i = 0; i < solutions.length; i++) {                    ///checks solutions array for possible wins
+  if(Math.abs(gameboard[solutions[i][0]] +                    ///checks first value in first array in the solutions 
+              gameboard[solutions[i][1]] +                   /// & compares it to the value put in the gameboard cube by player
+              gameboard[solutions[i][2]]) ===3){            ///checks and adds all cubes absolute values
+                return gameboard[solutions[i][0]];         ///which is either 1 or -1 and checks if it adds to 3
+              };                                          /// returns winner
+  };
+ if(gameboard.includes(null)) return false;
+ return 'T';
+};
 
 function handleClick(event) {
- const position = event.target.dataset.cube;
- if(gameboard[position] !== null) return;
+ const position = event.target.dataset.cube;      
+ if(winner || gameboard[position]) return;                    ///stops overlapping on cubes
  gameboard[position] = turn;
  turn *=-1;
+ winner = getWinner();
  render();
-
 };
 
 function render() {
-  squareEls.forEach(function(square, position) {
-   square.textContent = PLAYERS [gameboard[position]];
+  squareEls.forEach(function(square, position) {            ///funtion to put x's and o's
+   square.textContent = PLAYERS [gameboard[position]];     ///
   });
+  if(!winner) {                                                               ///From here below updates the
+  messageEl.textContent= `Players ${PLAYERS[turn]}'s turn`;
+ } else if(winner === 'T') {
+  messageEl.textContent = 'Tie Game!! Play Again!';
+ } else {
+  messageEl.textContent = `Player ${PLAYERS[winner]} Wins! Congradulations!!`;      
+ }
 };
 
 
